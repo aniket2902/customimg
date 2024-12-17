3925c087960dca9a71b34dbe76fe9de641c27e0f
Aniket <aniketmahajan19@gmail.com>
2024-12-17 18:44:21 +0530

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send("App Version 22.0")
+    response.send("App Version 24.0")
 })
 
 app.listen(4000, '0.0.0.0', ()=>{
